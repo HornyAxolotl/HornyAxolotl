32551f58b5736c7ca922669ccdcbdbac42c339c9
HornyAxolotl <142561072+HornyAxolotl@users.noreply.github.com>
2024-04-09 15:50:39 -0400

    Update FPController.cs

--- FPController.cs
+++ FPController.cs
@@ -72,7 +72,7 @@ public class FPController : MonoBehaviour
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * lookSpeed);
         }
 
         #endregion
